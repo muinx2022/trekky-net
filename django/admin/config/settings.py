fd2ca2725fe074ea83a598e661f578ad0932a42c
muinx2022 <muinx2022@gmail.com>
2026-03-30 17:44:52 +0700

    Serve static files via WhiteNoise finders

--- django/admin/config/settings.py
+++ django/admin/config/settings.py
@@ -144,6 +144,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [ADMIN_APP_DIR / "static"]
+WHITENOISE_USE_FINDERS = True
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
